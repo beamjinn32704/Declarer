d4761c019ba6d8b66ef73b39689dc49cf1e47bb7
beamjinn32704 <beamjinn32704@outlook.com>
2022-09-24 19:38:38 -0500

    Removed AutoShortcut Feature

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 
-import java.awt.Desktop;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 
@@ -21,13 +18,9 @@ import javax.swing.UIManager;
 public class Main {
     
     public static MainFrame main;
-    private static String shortcutParent = "C:\\users\\" + System.getProperty("user.name") + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
-    private static String lnkName = "Declarer3.0";
-    private static String lnk;
-    private static String[] pastVers = new String[] { "Declarer.lnk", "Declarer2.0.lnk", "Declarer2.5.lnk", "Declarer2.7.lnk"};
     
     public static void main(String[] args) throws FileNotFoundException {
-        shortcut();
+
         File ctheme = new File("ct.dll");
         if(ctheme.isFile()){
             try(Scanner in = new Scanner(ctheme)){
@@ -58,57 +51,4 @@ public class Main {
         main.setVisible(true);
     }
     
-    private static void shortcut(){
-        delPastVers();
-        lnk = lnkName + ".lnk";
-        if(!(new File(shortcutParent, lnk)).exists()) {
-            File batch = new File("Declarer.bat");
-            try (PrintWriter writer = new PrintWriter(batch);){
-                writer.println("@echo off");
-                writer.println("set startupPath=\"c:\\users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\n"
-                        + "set exePath=\"%CD%\n"
-                        + "cd %startupPath%\n"
-                        + "echo Set oWS = WScript.CreateObject(\"WScript.Shell\") > %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo sLinkFile = \"" + lnk + "\" >> %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo oLink.TargetPath = %exePath%\\Declarer.exe\" >> %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo oLink.WorkingDirectory = %startupPath%\" >> %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo oLink.Description = \"" + lnkName + "\" >> %startupPath%\\CreateShortcut.vbs\n" +
-                        "echo oLink.Save >> %startupPath%\\CreateShortcut.vbs\n"
-                        + "cd %startupPath%\n" +
-                        "C:\\Windows\\System32\\cscript.exe %startupPath%\\CreateShortcut.vbs\n" +
-                        "del CreateShortcut.vbs\n"
-                        + "echo %CD%\n"
-                        + "start " + lnk +"\n"
-                        + "cd %exePath%\n" +
-                        "(goto) 2>nul & del \"%~f0\" & exit /b");
-                Desktop.getDesktop().open(batch);
-            } catch (Exception e){
-                boolean fail = false;
-                if(batch.exists()){
-                    try {
-                        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", batch.toString()});
-                    } catch(Exception ex){
-                        fail = true;
-                        JOptionPane.showMessageDialog(null, "Unable to set to open when computer starts!");
-                    }
-                } else {
-                    fail = true;
-                    JOptionPane.showMessageDialog(null, "Unable to set to open when computer starts!");
-                }
-                if(fail){
-                    batch.delete();
-                }
-            }
-        }
-    }
-    
-    private static void delPastVers() {
-        for(int i = 0; i < pastVers.length; i++) {
-        	File file = new File(shortcutParent, pastVers[i]);
-        	if(file.isFile()) {
-        		file.delete();
-        	}
-        }
-    }
 }
